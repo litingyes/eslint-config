2d86c73b52e4b134e2a81e5e72d0628de878d14e
liting <luz.liting@gmail.com>
2023-03-02 21:56:19 +0800

    feat(eslint-config-vue): init

---
+++ packages/eslint-config-vue/index.js
@@ -0,0 +1,8 @@
+const localpkg = require('local-pkg')
+
+const hasTS = localpkg.isPackageExists('typescript')
+const isVue2 = localpkg.getPackageInfo('vue').startsWith('2')
+
+module.exports = {
+    extends: [hasTS ? 'liting-yes/eslint-config-ts' : '@liting-yes/eslint-config-base',isVue2 ? 'plugin:vue/recommended' : 'plugin:vue/vue3-recommended' ]
+}
