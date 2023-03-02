19965a6c3d2ff6f1f199a2e22ca4af0d7c24c6b7
liting <luz.liting@gmail.com>
2023-03-02 21:49:50 +0800

    feat(eslint-config-react): init

---
+++ packages/eslint-config-react/index.js
@@ -0,0 +1,7 @@
+const localpkg = require('local-pkg')
+
+const hasTS = localpkg.isPackageExists('typescript')
+
+module.exports = {
+    extends: [hasTS ? '@liting-yes/eslint-config-ts' : '@liting-yes/eslint-config-base', 'plugin:react/recommended', 'plugin:react-hooks/recommended', 'plugin:jsx-a11y/recommended']
+}
