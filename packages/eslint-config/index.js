bb090b8309789c03ebb319bfe4857d5ddff06f09
liting <luz.liting@gmail.com>
2023-03-02 22:55:05 +0800

    feat(eslint-config): config

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,3 +1,9 @@
+const localpkg = require('local-pkg')
+
+const hasVue = localpkg.isPackageExists('vue')
+const hasReact = localpkg.isPackageExists('react')
+const hasTs = localpkg.isPackageExists('typescript')
+
 module.exports = {
-  extends: ['@liting-yes/eslint-config-ts'],
+  extends: [hasVue ? '@liting-yes/eslint-config-vue' : '',hasReact ? '@liting-yes/eslint-config-react' : '', (!hasVue && !hasReact && hasTs) ? '@liting-yes/eslint-config-ts' : '@liting-yes/eslint-config-base'],
 }
