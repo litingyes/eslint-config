a8e69f479de4df90070092d2532e6978a9ec25cd
liting <luz.liting@gmail.com>
2023-03-02 23:08:54 +0800

    feat(eslint-config-ts): add eslint-plugin-import

--- packages/eslint-config-ts/index.js
+++ packages/eslint-config-ts/index.js
@@ -1,10 +1,16 @@
 module.exports = {
-  extends: ['@liting-yes/eslint-config-base', 'plugin:@typescript-eslint/recommended'],
+  extends: ['@liting-yes/eslint-config-base', 'plugin:@typescript-eslint/recommended', 'plugin:import/recommended', 'plugin:import/typescript'],
   plugins: ['@typescript-eslint'],
   overrides: [
     {
       files: ['*.ts'],
       parser: '@typescript-eslint/parser',
+      settings: {
+        'import/resolver': {
+          typescript:true,
+          node: true
+        }
+      }
     },
   ],
 }
