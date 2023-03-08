84f35fa28125855639597184a0b429a304f913e9
liting <luz.liting@gmail.com>
2023-03-08 23:41:04 +0800

    fix(eslint-config): get version for vue

--- packages/eslint-config-vue/index.js
+++ packages/eslint-config-vue/index.js
@@ -1,8 +1,11 @@
 const localpkg = require('local-pkg')
 
 const hasTS = localpkg.isPackageExists('typescript')
-const isVue2 = localpkg.getPackageInfo('vue').startsWith('2')
+const isVue2 = localpkg.getPackageInfo('vue')?.version?.startsWith('2')
 
 module.exports = {
-    extends: [hasTS ? 'liting-yes/eslint-config-ts' : '@liting-yes/eslint-config-base',isVue2 ? 'plugin:vue/recommended' : 'plugin:vue/vue3-recommended' ]
+  'extends': [
+    hasTS ? 'liting-yes/eslint-config-ts' : '@liting-yes/eslint-config-base',
+    isVue2 ? 'plugin:vue/recommended' : 'plugin:vue/vue3-recommended',
+  ],
 }
