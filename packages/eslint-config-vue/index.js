58d1ebe4ec0f14ba04351cbd8f005c5f18bb3876
liting <luz.liting@gmail.com>
2023-03-11 23:03:01 +0800

    perf(eslint-config-vue): use @typescript-eslint/parser as parser

--- packages/eslint-config-vue/index.js
+++ packages/eslint-config-vue/index.js
@@ -8,4 +8,11 @@ module.exports = {
     hasTS ? '@liting-yes/eslint-config-ts' : '@liting-yes/eslint-config-base',
     isVue2 ? 'plugin:vue/recommended' : 'plugin:vue/vue3-recommended',
   ],
+  overrides: [
+    {
+      files:         [ '*.vue' ],
+      parser:        'vue-eslint-parser',
+      parserOptions: { parser: '@typescript-eslint/parser' },
+    },
+  ],
 }
